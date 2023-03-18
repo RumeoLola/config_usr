869c4aa7fa1a0b1d83bc0292e78cb83ceccea5a6
RumeoLola <rumeomololaz@gmail.com>
2023-03-17 21:55:26 -0400

    modified code to be more organised

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import socket
 import re, uuid
 
 
-# get hostname
-hostname = socket.gethostname()
+def usr_information():
+    # get hostname
+    hostname = socket.gethostname()
+    # get ip address
+    ip_address = socket.gethostbyname(hostname)
+    # printing the value of unique MAC
+    # address using uuid and getnode() function
+    mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+    print(hostname, ip_address, mac_address)
 
 
-# get ip address
-ip_address = socket.gethostbyname(hostname)
-
-
-print(hostname)
-print(ip_address)
-
-
-# printing the value of unique MAC
-# address using uuid and getnode() function
-mac_address = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
-print(mac_address)
+usr_information()
